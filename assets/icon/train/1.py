758fee15b964c2528dea883a2e49edd5c324780d
CrYinLang <11822174+CrYinLang@user.noreply.gitee.com>
2026-02-03 16:45:45 +0800

    自动同步 [03 周二 -02-2026 16:45]

--- assets/icon/train/1.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import os
-
-# 获取当前目录下的所有文件
-for filename in os.listdir('.'):
-    if filename.endswith('.png'):
-        img = Image.open(filename)
-        img = img.resize((256, 256))
-        img.save(filename)
